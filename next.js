bc3156dadf0b3653413b20f7020c45ebe0e56e9f
Arnaud Leymet <aleymet@bouyguestelecom.fr>
2024-03-14 18:35:08 +0100

    :bug: Fix bytel stats query

--- next.js
+++ next.js
@@ -1,4 +1,4 @@
 module.exports = [
-  'fully supports es6-module AND fully supports cryptography AND >= 0.2% in my stats AND last 3 years AND fully supports css-color-function',
+  'fully supports es6-module AND fully supports cryptography AND >= 0.2% in browserslist-config-bouyguestelecom stats AND last 3 years AND fully supports css-color-function',
   'not dead',
 ]
